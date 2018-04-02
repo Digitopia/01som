8d927c30ad2dbc7d041853408b6bebbc84cc1080
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2018-04-02 17:35:09 +0200

    again

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+let config = {
     launch: {
-        headless: false,
+        headless: process.env.CI,
         devtools: true,
         args: ["--window-size=1280,1024"]
     }
 }
+
+if (!process.env.CI) config.launch.executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+
+module.exports = config
