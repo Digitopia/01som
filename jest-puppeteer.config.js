20eacdd51311ebda578b2b6b49ccc89d377ac335
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2018-04-02 16:55:41 +0200

    don't override executable path in travis

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     launch: {
-        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
         headless: false,
         devtools: true,
         args: ["--window-size=1280,1024"]
